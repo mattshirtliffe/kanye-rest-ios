109172f61152518805683c416f279f11c5379b25
Matthew Shirtliffe <matt.shirtliffe@gmail.com>
2019-05-08 18:29:47 +0100

    Initial Commit

---
+++ Kanye Rest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kanye Rest
+//
+//  Created by Matthew Shirtliffe on 08/05/2019.
+//  Copyright © 2019 Space Labs Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
